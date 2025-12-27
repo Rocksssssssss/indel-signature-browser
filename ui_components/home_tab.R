44e3202235aa6a48a005f60f6aaf62256c94109e
Steven G. Rozen <steverozen@pm.me>
2025-12-26 19:01:09 -0500

    adjusting text

--- ui_components/home_tab.R
+++ ui_components/home_tab.R
@@ -20,7 +20,22 @@ create_home_tab <- function() {
                "add or delete small sequences of DNA (conventionally < ~50 base pairs long).",
                "There is no single intuitive and naturally",
                "constrained classification of indel mutation types (as there arguably is for single base mutations)",
-               "but two classifications are useful and widely used:"
+               "but two classifications are useful and widely used."
+            ),
+            style = h3_style
+         ),
+         h3(
+            paste(
+               "This web site presents signatures extracted from somatic indel mutations in 6,975 tumors from 32 cancer types using ",
+               "methods based on non-negative factorization and hierarchical Dirichelet processes."
+            ),
+            style = h3_style
+         ),
+         h3(
+            paste(
+               "There is no single intuitive and naturally",
+               "constrained classification of indel mutation types (as there arguably is for single base mutations)",
+               "but two classifications have beeen useful."
             ),
             style = h3_style
          ),
@@ -28,7 +43,7 @@ create_home_tab <- function() {
             style = h3_style,
             tags$li(
                tags$a(
-                  tags$strong("COSMIC83:"),
+                  tags$strong("83-type classification scheme:"),
                   " ",
                   icon("link", style = "font-size:14px;"),
                   href = "#",
@@ -36,13 +51,13 @@ create_home_tab <- function() {
                   style = "color:#3498db; text-decoration:none; cursor:pointer; transition: color 0.3s;"
                ),
                paste(
-                  " The most widely used classification, which classifies indels into 83 types.",
+                  " This widely used classification scheme recognizes 83 types of indel.",
                   "Described in Alexandrov et al., 2020 and used on the COSMIC mutational signatures web site."
                )
             ),
             tags$li(
                tags$a(
-                  tags$strong("Koh89:"),
+                  tags$strong("89-type classification scheme:"),
                   " ",
                   icon("link", style = "font-size:14px;"),
                   href = "#",
@@ -50,22 +65,12 @@ create_home_tab <- function() {
                   style = "color:#3498db; text-decoration:none; cursor:pointer; transition: color 0.3s;"
                ),
                paste(
-                  " New indel classifiction scheme (Koh et al., 2025), which classifies indels into 89 types.",
-                  "Provides more informative granularity for indels in homopolymers (e.g. ATTTTTG → ATTTTG).",
-                  "Koh et al., 2025 also present an even more granular classifiction of 476 mutation types."
+                  " This new indel classifiction scheme (Koh et al., 2025) recognizes 89 types of indel.",
+                  "Often provides more informative granularity for indels in homopolymers (e.g. ATTTTTG → ATTTTG).",
+                  "Koh et al., 2025 also present an even more granular classifiction of 476 types of indel."
                )
             )
-         ),
-         h3(
-            paste(
-               "TEMP TEST With a few exceptions, the reference signatures on this web site were extracted using both",
-               "SigProfiler (as described in Alexandrov et al., 2020) and mSigHdp (Liu et al., 2023) from the",
-               "2,780 whole-genome from ICGC/TCGA Pan Cancer Analysis of Whole Genomes (PCAWG) Network and",
-               ">4,000 whole-genome from Hartwig Medical Foundation."
-            ),
-            style = h3_style
          )
       )
    )
 }
-
